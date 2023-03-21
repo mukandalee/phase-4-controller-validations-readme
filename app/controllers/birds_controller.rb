7c56b3d14c4ac4d12c9b5016bf1ef251894a290e
mukandalee <leonardmukanda254@gmail.com>
2023-03-21 04:34:16 +0300

    completed the lab

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,8 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+   # added rescue_from
+   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
 
   # GET /birds
   def index
@@ -9,9 +12,12 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(bird_params)
+    # create! exceptions will be handled by the rescue_from ActiveRecord::RecordInvalid code
+    bird = Bird.create!(bird_params)
     render json: bird, status: :created
   end
+  
+  
 
   # GET /birds/:id
   def show
@@ -22,9 +28,11 @@ class BirdsController < ApplicationController
   # PATCH /birds/:id
   def update
     bird = find_bird
-    bird.update(bird_params)
+    # update! exceptions will be handled by the rescue_from ActiveRecord::RecordInvalid code
+    bird.update!(bird_params)
     render json: bird
   end
+  
 
   # DELETE /birds/:id
   def destroy
@@ -35,6 +43,12 @@ class BirdsController < ApplicationController
 
   private
 
+  def render_unprocessable_entity_response(invalid)
+    #render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+
+
+  end
   def find_bird
     Bird.find(params[:id])
   end
